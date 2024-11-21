cda10942fc2efc02dc24152f2601adcc155d1475
Vlad Osipov <vladyslav.osip@gmail.com>
2024-11-21 17:22:57 +0200

    add app and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import contactsData from "./components/Contact/contact.json";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // const [contacts, setContacts] = useState(contactsData);
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem("contacts");
+    return savedContacts ? JSON.parse(savedContacts) : contactsData;
+  });
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
+
+  //   console.log(contacts);
+
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const handleAddContact = (newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  };
+
+  const handleDeleteContact = (id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    );
+  };
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="containerApp">
+      <h1>Phonebook</h1>
+      <ContactForm onAddContact={handleAddContact} />
+      <SearchBox value={filter} onChange={handleFilterChange} />
+      <ContactList
+        contacts={filteredContacts}
+        onDeleteContact={handleDeleteContact}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import css from "./Contact.module.css";
+import { FaUser, FaPhone } from "react-icons/fa";
+
+const Contact = ({ name, number, onDelete }) => {
+  return (
+    <li className={css.li}>
+      <div className={css.containerLi}>
+        <div className={css.liInfo}>
+          <FaUser className={css.icon} />
+          <span className={css.name}>{name}</span>
+        </div>
+        <div className={css.liInfo}>
+          <FaPhone className={css.icon} />
+          <span className={css.number}>{number}</span>
+        </div>
+      </div>
+      {/* {name}: {number} */}
+      <button className={css.btn} onClick={onDelete}>
+        Delete
+      </button>
+    </li>
+  );
+};
+
+export default Contact;
